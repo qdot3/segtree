a248a297c01d343e9aa04c3624dc746fc240ced0
qdot3 <quantum.engine.55g@gmail.com>
2026-06-02 18:33:18 +0900

    modified

--- crates/segtree_lazy/examples/library_checker.rs
+++ crates/segtree_lazy/examples/library_checker.rs
@@ -1,38 +1,46 @@
 /// Verified at <https://judge.yosupo.jp/problem/range_affine_range_sum>
 use std::io::{stdin, stdout, BufWriter, Write};
 
-use input::{bind, parse, FastInput};
 use op_add::OpAdd;
 use output::IntBuffer;
+use reader::FastBufReader;
 use segtree_lazy::LazySegtree;
 use traits::{Identity, SemiGroup};
 
 const MOD: u64 = 998_244_353;
 
 fn main() {
-    let mut input: FastInput<std::io::StdinLock<'_>> = FastInput::new(stdin().lock());
-    let mut output = BufWriter::with_capacity(1 << 16, stdout().lock());
+    let mut input = FastBufReader::<{ 1 << 16 }, _>::new(stdin().lock());
+    let mut output = BufWriter::with_capacity(1 << 18, stdout().lock());
     let mut buf = IntBuffer::new();
 
-    bind! { input >> n: usize, q: usize, }
+    let n: usize = input.parse_next_token().unwrap();
+    let q: usize = input.parse_next_token().unwrap();
 
     type Update = OpAffine;
     type Query = (OpAdd<u64>, OpAdd<u64>);
 
     let mut lst = LazySegtree::<Update, Query, _>::from_iter(
         |(a, b), (sum, n)| ((a * (sum % MOD) + b * n) % MOD, n),
-        (0..n).map(|_| (parse!(input >> u64), 1)),
+        (0..n).map(|_| {
+            let a: u64 = input.parse_next_token().unwrap();
+            (a, 1)
+        }),
     );
 
     for _ in 0..q {
-        bind! { input >> t: u8, }
+        let t: u8 = input.parse_next_token().unwrap();
 
         if t == 0 {
-            bind! { input >> l: usize, r: usize, b: u64, c: u64, }
+            let l: usize = input.parse_next_token().unwrap();
+            let r: usize = input.parse_next_token().unwrap();
+            let b: u64 = input.parse_next_token().unwrap();
+            let c: u64 = input.parse_next_token().unwrap();
 
             lst.range_update(l..r, (b, c));
         } else {
-            bind! { input >> l: usize, r: usize, }
+            let l: usize = input.parse_next_token().unwrap();
+            let r: usize = input.parse_next_token().unwrap();
 
             let sum = lst.range_query(l..r).0 % MOD;
 
